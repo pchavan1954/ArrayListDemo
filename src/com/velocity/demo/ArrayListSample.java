95f7debcc9f5c083c7a288706c9735a343165b7f
pratham <ptchavan99@gmail.com>
2022-12-24 20:21:37 +0530

    first commit :added arraylist

---
+++ src/com/velocity/demo/ArrayListSample.java
@@ -0,0 +1,15 @@
+package com.velocity.demo;
+
+import java.util.ArrayList;
+
+public class ArrayListSample {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("ajay");
+		list.add("vijay");
+		list.add("sanjay");
+		System.out.println(list);
+}
+}
